a415bcf18623a2d264bf5d7b02963f2a8b8c8c74
kaneAvocado <teplo543@gmail.com>
2023-11-05 23:44:20 +0300

    Не правильно надо переделать логику

--- Resourse XML/Source.cpp
+++ Resourse XML/Source.cpp
@@ -1,100 +1,172 @@
 #include <iostream>
-#include <functional>
 #include <fstream>
-#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <stdexcept>
 
-class XMLNode {
-public:
-    using XMLNodePtr = std::shared_ptr<XMLNode>;
-
+class XmlResource {
 private:
-    std::string name;
-    std::string value;
-    std::vector<XMLNodePtr> children;
-    XMLNodePtr parent;
-
-public:
-    XMLNode(const std::string& nodeName, const std::string& nodeValue, XMLNodePtr parentNode = nullptr)
-        : name(nodeName), value(nodeValue), parent(std::move(parentNode)) {}
-
-    std::string getName() const { return name; }
-    std::string getValue() const { return value; }
-    const std::vector<XMLNodePtr>& getChildren() const { return children; }
-
-    XMLNodePtr getParent() const {
-        return parent;
-    }
-};
-
-// Простой класс для парсинга XML
-class XMLParser {
-public:
-    XMLNode::XMLNodePtr parseXML(const std::string& filename) {
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            throw std::runtime_error("Cannot open file");
-        }
-
-        XMLNode::XMLNodePtr root;
-        XMLNode::XMLNodePtr currentNode;
-
-        std::string line;
-        while (std::getline(file, line)) {
-            // Базовая обработка для открытия и закрытия тегов
-            if (line.find('<') != std::string::npos && line.find("</") == std::string::npos) {
-                size_t startPos = line.find('<') + 1;
-                size_t endPos = line.find('>');
-                std::string tagName = line.substr(startPos, endPos - startPos);
-                // Упрощенная логика: предполагается, что за открывающим тегом следует значение
-                std::getline(file, line);
-                std::string value = line;
-                auto node = std::make_shared<XMLNode>(tagName, value, currentNode);
-                if (!currentNode) {  // Это корневой узел
-                    root = node;
-                }
-                currentNode = node;  // Настраиваем текущий узел для добавления детей
-            }
-            else if (line.find("</") != std::string::npos) {
-                currentNode = currentNode->getParent();  // Возвращаемся к родителю
-            }
-        }
-
-        return root;
-    }
-};
-
-// Функция для сохранения XML
-void saveXML(const XMLNode::XMLNodePtr& root, const std::string& filename) {
-    std::ofstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("Cannot open file for writing");
-    }
-
-    std::function<void(const XMLNode::XMLNodePtr&, size_t)> writeNode;
-    writeNode = [&](const XMLNode::XMLNodePtr& node, size_t depth) {
-        std::string indent(depth, ' ');
-        file << indent << "<" << node->getName() << ">\n";
-        file << indent << "  " << node->getValue() << "\n";
-        for (const auto& child : node->getChildren()) {
-            writeNode(child, depth + 2);
-        }
-        file << indent << "</" << node->getName() << ">\n";
+    struct Node {
+        std::string name;
+        std::string value;
+        std::map<std::string, Node> children;
     };
 
-    writeNode(root, 0);
-}
+    Node root;
+
+    Node parseNode(std::istringstream& iss) {
+        Node node;
+        std::string line;
+        while (std::getline(iss, line)) {
+            std::istringstream lineStream(line);
+            std::string tag;
+            if (line.find("<") != std::string::npos) {
+                std::getline(lineStream, tag, '>');
+                node.name = tag.substr(1, tag.find_first_of(" />") - 1);
+                if (line.find("</") != std::string::npos) {
+                    break;
+                }
+                else {
+                    node.children[node.name] = parseNode(iss);
+                }
+            }
+            else {
+                node.value = line;
+            }
+        }
+        return node;
+    }
+
+    void saveNode(std::ofstream& ofs, const Node& node, const std::string& indent) {
+        ofs << indent << "<" << node.name << ">\n";
+        if (!node.value.empty()) {
+            ofs << indent << "  " << node.value << "\n";
+        }
+        for (const auto& child : node.children) {
+            saveNode(ofs, child.second, indent + "  ");
+        }
+        ofs << indent << "</" << node.name << ">\n";
+    }
+
+public:
+    XmlResource() = default;
+
+    void loadFromFile(const std::string& filename) {
+        std::ifstream ifs(filename);
+        if (!ifs) {
+            throw std::runtime_error("Cannot open file for reading: " + filename);
+        }
+        std::string content((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+        std::istringstream iss(content);
+        root = parseNode(iss);
+    }
+
+    void saveToFile(const std::string& filename) {
+        std::ofstream ofs(filename);
+        if (!ofs) {
+            throw std::runtime_error("Cannot open file for writing: " + filename);
+        }
+        saveNode(ofs, root, "");
+    }
+
+    using NodePtr = Node*;
+
+    std::string trim(const std::string& str) {
+        auto start = str.begin();
+        while (start != str.end() && std::isspace(*start)) {
+            start++;
+        }
+
+        auto end = str.end();
+        do {
+            end--;
+        } while (std::distance(start, end) > 0 && std::isspace(*end));
+
+        return std::string(start, end + 1);
+    }
+
+
+    NodePtr find(const std::string& name, const std::string& value, NodePtr node = nullptr) {
+        if (node == nullptr) {
+            node = &root;
+        }
+        if (node->name == name && trim(node->value) == trim(value)) {
+            return node;
+        }
+        for (auto& child : node->children) {
+            NodePtr result = find(name, value, &(child.second));
+            if (result != nullptr) {
+                return result;
+            }
+        }
+        return nullptr;
+    }
+
+    NodePtr add(const std::string& name, const std::string& value, NodePtr parent) {
+        if (parent == nullptr) {
+            throw std::invalid_argument("Parent node is null");
+        }
+        Node& newNode = parent->children[name];
+        newNode.name = name;
+        newNode.value = value;
+        return &newNode;
+    }
+
+    bool eraseHelper(NodePtr parentNode, NodePtr nodeToDelete) {
+        if (parentNode == nullptr || nodeToDelete == nullptr) {
+            return false;
+        }
+
+        for (auto it = parentNode->children.begin(); it != parentNode->children.end(); ++it) {
+            if (&(it->second) == nodeToDelete) {
+                parentNode->children.erase(it);
+                return true;
+            }
+            else {
+                if (eraseHelper(&(it->second), nodeToDelete)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool erase(NodePtr nodeToDelete) {
+        if (nodeToDelete == nullptr || nodeToDelete == &root) return false;
+        return eraseHelper(&root, nodeToDelete);
+    }
+};
 
 int main() {
+    XmlResource xmlRes;
+
     try {
-        XMLParser parser;
-        auto root = parser.parseXML("xmlData.txt");
-        saveXML(root, "output.xml");
+        xmlRes.loadFromFile("example.xml");
+
+        XmlResource::NodePtr foundNode = xmlRes.find("first.potomok", "my value of first.potomok");
+        if (foundNode) {
+            std::cout << "Found node: " << foundNode->name << " with value: " << foundNode->value << std::endl;
+        }
+        else {
+            std::cout << "Node not found." << std::endl;
+        }
+
+        XmlResource::NodePtr addedNode = xmlRes.add("new.child", "new value", foundNode);
+        if (addedNode) {
+            std::cout << "Added node: " << addedNode->name << " with value: " << addedNode->value << std::endl;
+        }
+
+        bool erased = xmlRes.erase(addedNode);
+        std::cout << "Node " << (erased ? "erased successfully." : "was not erased.") << std::endl;
+
+        xmlRes.saveToFile("example_modified.xml");
+        std::cout << "XML file was saved successfully." << std::endl;
+
     }
     catch (const std::exception& e) {
-        std::cerr << "An error occurred: " << e.what() << '\n'; // stron exeption garanty
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
     return 0;
